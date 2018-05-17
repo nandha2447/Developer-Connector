90536efb5b5620b16cbd09f5fc730d234b74bc22
Nandha Kumar <nandha2447@gmail.com>
2018-05-17 13:14:04 +0530

    Connecting to MongoDB with mongoose

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+  mongoURI: "mongodb://nandha:nandha@ds119160.mlab.com:19160/devconnector"
+};
--- server.js
+++ server.js
@@ -1,8 +1,19 @@
 //This file is the starting point for the application
 
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
+
+//DB config
+
+const db = require("./config/keys").mongoURI;
+
+//connect to MongoDB
+mongoose
+  .connect(db)
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.log(err));
 
 app.get("/", (req, res) => res.send("Hello world haha"));
 
